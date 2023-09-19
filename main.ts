1fe85d726e447db46fe4704fb948d98634e38328
Calligramm <126103740+Calligramm@users.noreply.github.com>
2023-09-19 18:17:40 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,19 @@
+radio.onReceivedValue(function (name, value) {
+    if (name == "x") {
+        xWert = value
+        xWert = xWert - 513
+    }
+    if (name == "y") {
+        yWert = value
+        yWert = yWert - 513
+    }
+})
+let yWert = 0
+let xWert = 0
+radio.setGroup(2)
+let Motor_a = l298.create_motor(DigitalPin.P0, DigitalPin.P1, AnalogPin.C16)
+let motor_b = l298.create_motor(DigitalPin.P2, DigitalPin.P3, AnalogPin.C17)
+let vehicle = l298.create_vehicle(Motor_a, motor_b)
+basic.forever(function () {
+    vehicle.drive(yWert, xWert)
+})
