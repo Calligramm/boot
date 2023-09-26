758ea17ca3562dd0d6f7b8efbd61e3e9443b47bc
Calligramm <126103740+Calligramm@users.noreply.github.com>
2023-09-26 18:03:39 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    Knopfdruck = receivedNumber
+})
 radio.onReceivedValue(function (name, value) {
     if (name == "x") {
         xWert = value
@@ -7,13 +10,21 @@ radio.onReceivedValue(function (name, value) {
         yWert = value
         yWert = yWert - 513
     }
+    if (name == "z") {
+        Knopfdruck = value
+    }
 })
 let yWert = 0
 let xWert = 0
+let Knopfdruck = 0
 radio.setGroup(2)
 let Motor_a = l298.create_motor(DigitalPin.P0, DigitalPin.P1, AnalogPin.C16)
 let motor_b = l298.create_motor(DigitalPin.P2, DigitalPin.P3, AnalogPin.C17)
 let vehicle = l298.create_vehicle(Motor_a, motor_b)
 basic.forever(function () {
     vehicle.drive(yWert, xWert)
+    if (Knopfdruck == 1) {
+        Motor_a.stop()
+        motor_b.stop()
+    }
 })
